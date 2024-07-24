9cd383f40fb559d12373b3c5e8e04d0297bd161e
OrlandoYV <sergioyv@gmail.com>
2024-07-23 19:15:55 -0500

    oy - layout list heroes

--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-layout-page',
   templateUrl: './layout-page.component.html',
-  styles: ``
+  styles: `
+
+  `,
 })
 export class LayoutPageComponent {
 
+  public sidebarItems = [
+    { label: 'List' , icon: 'label', url: './list'},
+    { label: 'Add' , icon: 'add' , url: './new-hero'},
+    {label: 'Search', icon: 'search', url:'./search' },
+
+  ]
 }
