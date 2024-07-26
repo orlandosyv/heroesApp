af54d1e322cbb523bdac1e8e56b9de4ed7b02add
OrlandoYV <sergioyv@gmail.com>
2024-07-25 23:00:59 -0500

    oy - getter

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Hero, Publisher } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-new-page',
@@ -7,8 +10,34 @@ import { Component } from '@angular/core';
 })
 export class NewPageComponent {
 
+  constructor(private heroesService: HeroesService ) {}
+
+  public heroForm = new FormGroup({
+    id:  new FormControl(''),
+    superhero: new FormControl('', {nonNullable: true}),
+    publisher:  new FormControl<Publisher>(Publisher.DCComics),
+    alter_ego:  new FormControl(''),
+    first_appearance: new FormControl(''),
+    characters: new FormControl(''),
+    alt_img: new FormControl(''),
+  } )
+
   public publishers = [
     { id: 'DC Comics', desc: 'DC - Comics' },
     { id: 'Marvel Comics', desc: 'Marvel - Comics' },
   ];
+
+  get currentHero(): Hero {
+    const hero = this.heroForm.value as Hero;
+    return hero;
+  }
+
+  onSubmit(): void {
+    if (this.heroForm.invalid) return;
+
+    console.log({
+      formIsValid: this.heroForm.valid,
+      value: this.heroForm.value,
+    })
+  }
 }
