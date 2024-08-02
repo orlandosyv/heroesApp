50a9e4b19f32fe1fa2d306877595b2c3baf95482
OrlandoYV <sergioyv@gmail.com>
2024-08-01 23:04:44 -0500

    oy - user display on layout getter an onLogout

--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../../auth/services/auth.service';
+import { User } from '../../../auth/interfaces/user.interface';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-layout-page',
@@ -17,10 +19,18 @@ export class LayoutPageComponent {
   ]
 
   constructor(
-    private authService: AuthService
+    private authService: AuthService,
+    private router: Router
   ) { }
 
+  get user(): User | undefined {
+    return this.authService.currentUser;
+
+  }
+
   onLogout() {
-    this.authService.logout()
+    this.authService.logout();
+    this.router.navigate(['auth/login'])
+
   }
 }
